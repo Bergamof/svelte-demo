268cb203c311d8bd25fa8469342c2ccc2e7e8ef8
Adrien Bouchardon <abouchardon@ippon.fr>
2020-10-13 15:52:12 +0200

    Implement Angular TaskCard component

--- AngularTasklist/src/app/task-card/task-card.component.ts
+++ AngularTasklist/src/app/task-card/task-card.component.ts
@@ -1,16 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Task } from '../entities/task';
+import { TaskService } from '../shared/task-service';
+import { UserService } from '../shared/user-service';
 
 @Component({
 	selector: 'app-task-card',
-	templateUrl: './task-card.component.html',
-	styleUrls: ['./task-card.component.css']
+	template: `
+		<li class="task">
+			<label for="completion">
+				<input
+					id="completion"
+					type="checkbox"
+					[checked]=task.completed
+					(change)="changeTaskCompletion()" />
+				{{task.completed ? 'Completed' : 'In progress'}}
+			</label>
+
+			<h1>{{task.id}} - {{task.title}}</h1>
+			<div>{{taskUserName}}</div>
+		</li>
+	`,
+	// styleUrls: ['./task-card.component.css'],
+	styles: [`
+		.task {
+			background: aquamarine;
+			border: 2px solid #cccccc;
+			border-radius: 10px;
+
+			list-style-type: none;
+
+			margin: 5px;
+			padding: 10px;
+			width: 500px;
+		}
+	`]
 })
 export class TaskCardComponent implements OnInit {
 
-	constructor() {
+	@Input()
+	public task: Task = null;
+
+	public taskUserName = '';
+
+	constructor(private taskService: TaskService, private userService: UserService) {
 	}
 
 	ngOnInit(): void {
+		this.userService.getAll().subscribe(userList => {
+			this.taskUserName = userList.find(user => user.id === this.task.assigneeId)?.name ?? '';
+		});
 	}
 
+	public changeTaskCompletion(): void {
+		this.taskService.changeTaskCompletion(this.task, !this.task.completed);
+	}
 }
