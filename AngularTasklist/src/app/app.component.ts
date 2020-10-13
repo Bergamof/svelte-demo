bfbdb177753b16bc679e943216264b861f229082
Adrien Bouchardon <abouchardon@ippon.fr>
2020-10-13 17:01:16 +0200

    Add main nav

--- AngularTasklist/src/app/app.component.ts
+++ AngularTasklist/src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+	template: `
+		<nav>
+			<a href="/">Home</a>
+		</nav>
+		<router-outlet></router-outlet>
+	`
 })
 export class AppComponent {
 	title = 'AngularTasklist';
