224f58d6d57dacfd71a08c55db080d5040f9872a
Adrien Bouchardon <abouchardon@ippon.fr>
2020-10-14 14:47:01 +0200

    Implement Angular TaskList component

--- AngularTasklist/src/app/task-list/task-list.component.ts
+++ AngularTasklist/src/app/task-list/task-list.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../shared/task-service';
 
 @Component({
 	selector: 'app-task-list',
-	templateUrl: './task-list.component.html',
-	styleUrls: ['./task-list.component.css']
+	template: `
+		<h1>Angular TaskList</h1>
+
+		<a routerLink="/tasks/new">Create a task</a>
+
+		<ng-container *ngIf="taskService.getAll()|async as taskList">
+			<ng-container *ngIf="taskList.length>0; else notask">
+				<ul *ngFor="let task of taskList">
+					<app-task-card [task]="task"></app-task-card>
+				</ul>
+			</ng-container>
+		</ng-container>
+
+		<ng-template #notask>
+			<div>No task</div>
+		</ng-template>
+	`
 })
 export class TaskListComponent implements OnInit {
 
-	constructor() {
+	constructor(public taskService: TaskService) {
 	}
 
 	ngOnInit(): void {
